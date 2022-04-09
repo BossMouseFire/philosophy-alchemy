1245724eddabb604b73f8cdf470f7d562afe585f
BossMouseFire <abanin-02@mail.ru>
2022-04-09 18:23:36 +0400

    Added modal of about

--- src/components/history/history.tsx
+++ src/components/history/history.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './history.module.scss';
 import { Information } from '../information/information';
 import { useGlobalContext } from '../../context';
+import { ModalAbout } from '../modalAbout/modalAbout';
 
 export const History: React.FC = () => {
   const { history } = useGlobalContext();
   const { title, years, description } = history;
+  const [activeModal, setActiveModal] = useState<boolean>(false);
+
+  const onActiveModalAbout = () => {
+    setActiveModal(true);
+  };
 
   return (
     <div className={styles.history}>
@@ -14,8 +20,9 @@ export const History: React.FC = () => {
       </div>
       <Information title={title} years={years} description={description} />
       <div className={styles.actions}>
-        <button>О проекте</button>
+        <button onClick={onActiveModalAbout}>О проекте</button>
       </div>
+      {activeModal && <ModalAbout activeModal={activeModal} setActiveModal={setActiveModal} />}
     </div>
   );
 };
